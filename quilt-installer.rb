f3ee8141c4d05faf9fd8287a76bad79e2cc26289
Southpaw <git@southpaw1496.dev>
2022-11-17 21:30:33 +0000

    OpenSSL goes above Rust, apparently

--- quilt-installer.rb
+++ quilt-installer.rb
@@ -5,8 +5,8 @@ class QuiltInstaller < Formula
   sha256 "42d80580dc11e3a883b06633450f8121de704cb0ba24ac56d067f898473eddaa"
   license "Apache-2.0"
 
-  depends_on "rust" => :build
   depends_on "openssl" => :build
+  depends_on "rust" => :build
 
   def install
     system "cargo", "install", *std_cargo_args
