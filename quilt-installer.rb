614f930e7a9727e9fb14fb07016b36c94f71b1cd
Southpaw <git@southpaw1496.dev>
2022-11-17 16:39:49 +0000

    quilt-installer 0.1.1 (new formula)
    * Quilt Installer 0.1.1
    * Add OpenSSL Dependency
    * OpenSSL goes above Rust, apparently
    * Pin OpenSSL to 1.1
    * Add PKGConfig dependency
    * OpenSSL is now a "real" dependency
    * Three Cheers for fussy style checks
    * Four cheers...
    
    Closes #2.
    
    Signed-off-by: Southpaw <44805409+Southpaw1496@users.noreply.github.com>

---
+++ quilt-installer.rb
@@ -0,0 +1,20 @@
+class QuiltInstaller < Formula
+  desc "Native installer for the Quilt loader"
+  homepage "https://github.com/QuiltMC/quilt-native-installer"
+  url "https://github.com/QuiltMC/quilt-native-installer/archive/refs/tags/v0.1.1.tar.gz"
+  sha256 "42d80580dc11e3a883b06633450f8121de704cb0ba24ac56d067f898473eddaa"
+  license "Apache-2.0"
+
+  depends_on "rust" => :build
+  depends_on "openssl@1.1"
+
+  def install
+    system "cargo", "install", *std_cargo_args
+    mv "#{bin}/quilt-installer", "#{bin}/quilt"
+  end
+
+  test do
+    # The installer does not yet have any native CLI functions, so proper tests will be added once it does.
+    system "true"
+  end
+end
