dc609ea4155ca1d1e504f5cedd8ec6a404b07c7e
Southpaw <git@southpaw1496.dev>
2022-11-17 15:36:25 +0000

    brew audit therapy

--- Formula/quilt.rb
+++ Formula/quilt.rb
@@ -1,31 +1,19 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Quilt < Formula
-  desc ""
-  homepage ""
+  desc "Native installer for the Quilt mod loader"
+  homepage "https://github.com/QuiltMC/quilt-native-installer"
   url "https://github.com/QuiltMC/quilt-native-installer/archive/refs/tags/v0.1.1.tar.gz"
   sha256 "42d80580dc11e3a883b06633450f8121de704cb0ba24ac56d067f898473eddaa"
-  license "Apache-2"
+  license "Apache-2.0"
 
   depends_on "rust" => :build
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
     system "cargo", "install", *std_cargo_args
-	mv "#{bin}/quilt-installer", "#{bin}/quilt"
+    mv "#{bin}/quilt-installer", "#{bin}/quilt"
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test quilt`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    # The Native Installer does not yet have CLI functionality. Proper tests will be added when it does.
     system "true"
   end
 end
