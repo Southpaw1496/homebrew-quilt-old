5ce0b8247be35645bce7d64c163b72c327ab9795
Southpaw <44805409+Southpaw1496@users.noreply.github.com>
2022-11-18 19:58:33 +0000

    quilt-native-installer: add 0.1.1 bottle.

--- Formula/quilt-native-installer.rb
+++ Formula/quilt-native-installer.rb
@@ -5,6 +5,12 @@ class QuiltNativeInstaller < Formula
   sha256 "42d80580dc11e3a883b06633450f8121de704cb0ba24ac56d067f898473eddaa"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://github.com/Southpaw1496/homebrew-quilt/releases/download/quilt-native-installer-0.1.1"
+    sha256 cellar: :any_skip_relocation, monterey:     "dcbfe368c269765c403390f61a503d4e7c9d04ca4ec735f8c3ef4394745d26fe"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "ff8ea62a83959dbf8785eeea73e701ef61683a121e7a517fa2aa5d1f495b6543"
+  end
+
   depends_on "rust" => :build
   depends_on "openssl@1.1"
 
