ffea813ebe1265d55e228f507fa963d3de973c75
Southpaw <44805409+Southpaw1496@users.noreply.github.com>
2022-11-18 18:47:36 +0000

    quilt-installer: add 0.1.1 bottle.

--- quilt-installer.rb
+++ quilt-installer.rb
@@ -5,6 +5,12 @@ class QuiltInstaller < Formula
   sha256 "42d80580dc11e3a883b06633450f8121de704cb0ba24ac56d067f898473eddaa"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://github.com/Southpaw1496/homebrew-quilt/releases/download/quilt-installer-0.1.1"
+    sha256 cellar: :any_skip_relocation, monterey:     "5432fc00c67a9f2ceefc583991265e25c460107a927358df3a566e30adb6729c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "babebe15c4a4621291454cab96787f58709674b53d4d4e6a4e67f25155c70a3f"
+  end
+
   depends_on "rust" => :build
   depends_on "openssl@1.1"
 
