021cf9bc1718437a5ad233561540fe08694d73ba
Southpaw <git@southpaw1496.dev>
2022-11-20 14:31:21 +0000

    Add Croc as a test formula

---
+++ Formula/croc.rb
@@ -0,0 +1,30 @@
+class Croc < Formula
+  desc "Securely send things from one computer to another"
+  homepage "https://github.com/schollz/croc"
+  url "https://github.com/schollz/croc/archive/v9.6.1.tar.gz"
+  sha256 "a7f0db2b52e44358beb2782412c955f2d5a63da72832b83de48739b1431cb19c"
+  license "MIT"
+  head "https://github.com/schollz/croc.git", branch: "master"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args(ldflags: "-s -w")
+  end
+
+  test do
+    port=free_port
+
+    fork do
+      exec bin/"croc", "relay", "--ports=#{port}"
+    end
+    sleep 1
+
+    fork do
+      exec bin/"croc", "--relay=localhost:#{port}", "send", "--code=homebrew-test", "--text=mytext"
+    end
+    sleep 1
+
+    assert_match shell_output("#{bin}/croc --relay=localhost:#{port} --overwrite --yes homebrew-test").chomp, "mytext"
+  end
+end
